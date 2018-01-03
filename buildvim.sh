7d37eba995e25cb7b9111873f9c156ac642b8a51
Jan Oppolzer <jan@oppolzer.cz>
2018-01-03 15:46:28 +0100

    clone_repository() -> get_plugin() & fixes

--- buildvim.sh
+++ buildvim.sh
@@ -10,8 +10,19 @@ PLUGINS=$VIM/plugins
 mkdir -p $GITHUB
 mkdir -p $VIM $AUTOLOAD $BUNDLE $COLORS $PLUGINS
 
-function clone_repository() {
-    git clone -q $1 $BUNDLE/${1##*/}
+function get_plugin() {
+    DIR=${1#https://github.com/}
+
+    echo "Getting $DIR..."
+
+    if [ -d $GITHUB/$DIR ]; then
+        cd $GITHUB/$DIR &&
+            git pull
+    else
+        echo git clone $1 $BUNDLE/${1##*/}
+    fi
+
+    echo -e "Done.\n"
 }
 
 # pathogen
@@ -21,18 +32,18 @@ TMP=$GITHUB/tpope/vim-pathogen &&
     ln -s $TMP/autoload/pathogen.vim $AUTOLOAD/
 
 # get plugins
-clone_repository https://github.com/scrooloose/nerdtree
-clone_repository https://github.com/scrooloose/nerdcommenter
-clone_repository https://github.com/tomtom/tlib_vim
-clone_repository https://github.com/MarcWeber/vim-addon-mw-utils
-clone_repository https://github.com/vim-airline/vim-airline
-clone_repository https://github.com/vim-airline/vim-airline-themes
-clone_repository https://github.com/tpope/vim-fugitive
-clone_repository https://github.com/garbas/vim-snipmate
-clone_repository https://github.com/honza/vim-snippets
-clone_repository https://github.com/vimoutliner/vimoutliner
-clone_repository https://github.com/sukima/xmledit
-clone_repository https://github.com/luochen1990/rainbow
+get_plugin https://github.com/scrooloose/nerdtree
+get_plugin https://github.com/scrooloose/nerdcommenter
+get_plugin https://github.com/tomtom/tlib_vim
+get_plugin https://github.com/MarcWeber/vim-addon-mw-utils
+get_plugin https://github.com/vim-airline/vim-airline
+get_plugin https://github.com/vim-airline/vim-airline-themes
+get_plugin https://github.com/tpope/vim-fugitive
+get_plugin https://github.com/garbas/vim-snipmate
+get_plugin https://github.com/honza/vim-snippets
+get_plugin https://github.com/vimoutliner/vimoutliner
+get_plugin https://github.com/sukima/xmledit
+get_plugin https://github.com/luochen1990/rainbow
 
 # get solarized theme
 wget -q -P $COLORS https://raw.githubusercontent.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim
